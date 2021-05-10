0103b1267f1f98fe03ea9d0b502808d7bf54a509
HWNAGJIWON <41350477+HWNAGJIWON@users.noreply.github.com>
2021-05-10 18:38:25 +0900

    Update ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,11 +6,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	int		j;
 	char		*answer;
 	
-	i = 0;
-	while (s1[i])
-		i++;
-	while(s2[j])
-		j++;
+	i = strlen(s1);
+	j = strlen(s2);
 	if (!(answer = (char *)malloc(sizeof(char) * (i + j + 1))))
 		return (0);
 	i = 0;
